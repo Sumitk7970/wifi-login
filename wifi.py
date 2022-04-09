c7cd28da6ee3ad15d9533589a2e4075e0d1b92cf
Sumitk7970 <kumarsumit8210@gmail.com>
2022-04-10 02:18:26 +0530

    First commit

---
+++ wifi.py
@@ -0,0 +1,28 @@
+from time import sleep
+from selenium import webdriver
+
+USERNAME = 'your_user_name'
+PASSWORD = 'your_password'
+URL = '' # url of the login page
+
+def login(username, password):
+    usernameField = driver.find_element_by_xpath('//*[@id="ft_un"]')
+    usernameField.send_keys(username)
+
+    passwordField = driver.find_element_by_xpath('//*[@id="ft_pd"]')
+    passwordField.send_keys(password)
+
+    submitButton = driver.find_element_by_xpath('/html/body/div/div/form/div[3]/input')
+    submitButton.click()
+
+
+op = webdriver.ChromeOptions()
+op.add_argument('headless')
+driver = webdriver.Chrome(options=op)
+
+try:
+    driver.get(URL)
+    sleep(2) # wait for the page to load
+    login(USERNAME, PASSWORD)
+finally:
+    driver.quit()
